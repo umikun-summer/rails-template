7141ce1dc8f81c964c5de2c589ed38b141f60385
zackey <127865584+umikun-summer@users.noreply.github.com>
2025-08-17 00:22:55 +0900

    認証機能のセットアップを削除

--- template.rb
+++ template.rb
@@ -5,6 +5,15 @@ FILES = %w[
   config/locales/ja.yml
 ].freeze
 
+def setup_simple_form
+  simple_form_scaffold_template_file = 'lib/templates/haml/scaffold/_form.html.haml'
+  simple_form_scaffold_locale_file = 'config/locales/simple_form.en.yml'
+
+  generate 'simple_form:install', '--bootstrap'
+  remove_file simple_form_scaffold_template_file if File.exist?(simple_form_scaffold_template_file)
+  remove_file simple_form_scaffold_locale_file if File.exist?(simple_form_scaffold_locale_file)
+end
+
 def create_admins_application_controller
   file_path = 'app/controllers/admins/application_controller.rb'
 
@@ -17,6 +26,9 @@ FILES.each do |file_path|
   get BASE_REPOSITORY_URL % file_path, file_path
 end
 
+# en.yml は不要なので削除
+remove_file 'config/locales/en.yml'
+
 # 追加するgem
 gem 'sgcop', github: 'SonicGarden/sgcop', branch: 'main'
 gem 'haml_lint', require: false
@@ -24,33 +36,16 @@ gem 'simple_form'
 gem 'haml-rails'
 gem 'html2haml' # 一時的に使用する
 
-# en.yml は不要なので削除
-remove_file 'config/locales/en.yml'
-
 after_bundle do
   # Gemfile を自動修正
   run 'bundle exec rubocop -a Gemfile'
 
   # simple_form を bootstrapでセットアップ
-  generate 'simple_form:install', '--bootstrap'
-  simple_form_scaffold_template_file = 'lib/templates/haml/scaffold/_form.html.haml'
-  remove_file simple_form_scaffold_template_file if File.exist?(simple_form_scaffold_template_file)
-
-  # authentication = false
-  if yes?('認証機能を追加しますか？ [y/n]')
-    generate('authentication')
-
-    # authentication = true
-  end
-
-  # if authentication && yes?('OAuth用のmodelを追加しますか？ [y/n]')
-  #   generate('model', 'OauthAccount', 'user:references',' provider:string','uid:string','access_token:string', 'refresh_token:string', 'token_expires_at:datetime')
-  # end
+  setup_simple_form
 
   if yes?('管理者画面を追加しますか？ [y/n]')
     create_admins_application_controller
   end
-
 
   # erbをhamlに変換
   run "yes 'n' | bin/rails haml:erb2haml"
